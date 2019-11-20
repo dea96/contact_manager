2336d086c8a40c17160df37daf3d0a8de8269af7
dea96 <hariskaefendi1996@gmail.com>
2019-11-20 09:10:12 +0700

    backup

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,5 +1,44 @@
 class ContactsController < ApplicationController
   def index
-    @contacts = Contact.all
+      if params[:group_id] && !params[:group_id].empty?
+        #   @contacts = Contact.where(group_id: params[:group_id]).page(params[:page])
+          @contacts = Group.find(params[:group_id]).contacts.order(created_at: :desc).page(params[:page])
+      else
+          @contacts = Contact.order(created_at: :desc).page(params[:page])
+      end
+  end
+
+  def new
+      @contact = Contact.new
+  end
+
+  def create
+      @contact = Contact.new(contact_params)
+      if @contact.save
+          flash[:success] = "Contact was successfully created."
+          redirect_to contacts_path
+      else
+          render 'new'
+      end
+  end
+
+  def edit
+      @contact = Contact.find(params[:id])
+  end
+
+  def update
+      @contact = Contact.find(params[:id])
+      if @contact.update(contact_params)
+          flash[:success] = "Contact was successfully updated."
+          redirect_to contacts_path
+      else
+          render 'edit'
+      end
+  end
+
+  private
+
+  def contact_params
+      params.require(:contact).permit(:name, :email, :company, :address, :phone, :group_id)
   end
 end
